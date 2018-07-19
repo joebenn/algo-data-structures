b42c09933e18c7a75264d9a63224d4a6bbacb230
Joe Benn <jbenn@davis.co.nz>
2018-07-19 14:41:14 +1200

    more insertion methods

--- data-structures/linked_list.py
+++ data-structures/linked_list.py
@@ -7,17 +7,40 @@ class SLinkedList:
     def __init__(self):
         self.headval = None
     
+    #Print linked list
     def print_list(self):
         printval = self.headval
         while printval is not None:
             print(printval.dataval)
             printval = printval.nextval
     
+    #Add new node at begining of linked list.
     def addBegining(self, newData):
-        NewNode = Node(newData)
+        newNode = Node(newData)
         #Update the new nodes next val to existing node
-        NewNode.nextval = self.headval
-        self.headval = NewNode
+        newNode.nextval = self.headval
+        self.headval = newNode
+
+    #Add new node at the end of the linked list
+    def addEnd(self, newData):
+        newNode = Node(newData)
+        if self.headval is None:
+            self.headval = newNode
+            return
+        lastNode = self.headval
+        while(lastNode.nextval):
+            lastNode = lastNode.nextval
+        lastNode.nextval = newNode
+
+    #Add new node at position after 'preNode'
+    def addBetween(self, preNode, newData):
+        if preNode is None:
+            print("Node supplied is absent")
+            return
+        newNode = Node(newData)
+        newNode.nextval = preNode.nextval
+        preNode.nextval = newNode
+    
 
 list1 = SLinkedList()
 list1.headval = Node("Monday")
@@ -29,7 +52,9 @@ e2.nextval = e3
 
 list1.print_list()
 
-list1.addBegining("Sunday")
-print("-----")
+print("------")
+
+list1.addBetween(list1.headval, "Friday")
 
 list1.print_list()
+
