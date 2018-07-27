339f8445c8b2830b79cfefe8008191babef65284
Joe Benn <joebenn95@gmail.com>
2018-07-27 12:49:02 +1200

    added methods to graph.py

--- data-structures/graph.py
+++ data-structures/graph.py
@@ -1,6 +1,47 @@
-class Graph:
+class Graph(object):
 
     def __init__(self, graph=None):
         if graph == None:
             graph = {}
         self.__graph = graph
+
+    def add_node(self, node):
+        if node not in self.__graph:
+            self.__graph[node] = []
+
+    def add_edge(self, edge):
+        edge = set(edge)
+        (node1, node2) = tuple(edge)
+        if node1 in self.__graph:
+            self.__graph[node1].append(node2)
+        else:
+            self.__graph[node1] = [node2]
+
+    def nodes(self):
+        return list(self.__graph.keys())
+    
+    def edges(self):
+        edges = []
+        for node in self.__graph:
+            for neighbour in self.__graph[node]:
+                if (neighbour, node) not in edges:
+                    edges.append((node, neighbour))
+        return edges
+    
+    def __str__(self):
+        result = "Nodes: "
+        for node in self.nodes():
+            result += str(node) + " "
+        result += "\nEdges: "
+        for edge in self.edges():
+            result += str(edge) + " "
+        return result
+
+graph = { "A": ["B", "C"], "B": ["D", "E"], "C": ["A"], "D": ["B", 'E'], "E": ["B", "D"] }
+
+g = Graph(graph)
+print(g)
+
+g.add_node("F")
+g.add_edge(("C", "F"))
+print(g)
